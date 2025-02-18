da51f44a52f26ecf7a890da0e0d98e2786279f1f
Stanis Kapusta <51389578+TNtube@users.noreply.github.com>
2025-02-19 00:16:19 +0100

    Small enhancements

--- Core/include/DescriptorHeap.hpp
+++ Core/include/DescriptorHeap.hpp
@@ -13,16 +13,15 @@ namespace Zenyth
 
 		void operator += (INT offsetScaledByDescriptorSize );
 
-		const D3D12_CPU_DESCRIPTOR_HANDLE* operator&() const { return &m_cpuHandle; }
 		explicit operator D3D12_CPU_DESCRIPTOR_HANDLE() const { return m_cpuHandle; }
 		explicit operator D3D12_GPU_DESCRIPTOR_HANDLE() const { return m_gpuHandle; }
 
-		size_t GetCpuPtr() const { return m_cpuHandle.ptr; }
-		uint64_t GetGpuPtr() const { return m_gpuHandle.ptr; }
-		D3D12_CPU_DESCRIPTOR_HANDLE CPU() const { return m_cpuHandle; }
-		D3D12_GPU_DESCRIPTOR_HANDLE GPU() const { return m_gpuHandle; }
-		bool IsNull() const { return m_cpuHandle.ptr == 0; }
-		bool IsShaderVisible() const { return m_gpuHandle.ptr != 0; }
+		[[nodiscard]] size_t GetCpuPtr() const { return m_cpuHandle.ptr; }
+		[[nodiscard]] uint64_t GetGpuPtr() const { return m_gpuHandle.ptr; }
+		[[nodiscard]] D3D12_CPU_DESCRIPTOR_HANDLE CPU() const { return m_cpuHandle; }
+		[[nodiscard]] D3D12_GPU_DESCRIPTOR_HANDLE GPU() const { return m_gpuHandle; }
+		[[nodiscard]] bool IsNull() const { return m_cpuHandle.ptr == 0; }
+		[[nodiscard]] bool IsShaderVisible() const { return m_gpuHandle.ptr != 0; }
 
 	private:
 		D3D12_CPU_DESCRIPTOR_HANDLE m_cpuHandle;
@@ -38,23 +37,23 @@ namespace Zenyth
 		void Create(ID3D12Device *device, const std::wstring& debugHeapName, D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t maxCount);
 		void Destroy() { m_heap.Reset(); }
 
-		bool HasAvailableSpace( uint32_t count ) const;
+		[[nodiscard]] bool HasAvailableSpace( uint32_t count ) const;
 		DescriptorHandle Alloc( uint32_t count = 1 );
 
-		DescriptorHandle GetStartHandle() const { return m_firstHandle; }
+		[[nodiscard]] DescriptorHandle GetStartHandle() const { return m_firstHandle; }
 
 		DescriptorHandle operator[] (uint32_t arrayIdx) const;
-		uint32_t GetOffsetOfHandle(const DescriptorHandle& dHandle ) const;
-		ID3D12DescriptorHeap* GetHeapPointer() const { return m_heap.Get(); }
-		uint32_t GetDescriptorSize() const { return m_descriptorSize; }
+		[[nodiscard]] uint32_t GetOffsetOfHandle(const DescriptorHandle& dHandle ) const;
+		[[nodiscard]] ID3D12DescriptorHeap* GetHeapPointer() const { return m_heap.Get(); }
+		[[nodiscard]] uint32_t GetDescriptorSize() const { return m_descriptorSize; }
 
-		bool ValidateHandle( const DescriptorHandle& dHandle ) const;
+		[[nodiscard]] bool ValidateHandle( const DescriptorHandle& dHandle ) const;
 
 	private:
 		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_heap;
-		D3D12_DESCRIPTOR_HEAP_DESC m_heapDesc;
-		uint32_t m_descriptorSize;
-		uint32_t m_numFreeDescriptors;
+		D3D12_DESCRIPTOR_HEAP_DESC m_heapDesc {};
+		uint32_t m_descriptorSize {};
+		uint32_t m_numFreeDescriptors {};
 		DescriptorHandle m_firstHandle;
 		DescriptorHandle m_nextFreeHandle;
 	};
