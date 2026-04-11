05ebed1188e6e269b5baba965fb3578d65465a43
TNtube <tntube.dev@gmail.com>
2026-04-11 23:32:29 +0900

    Added base project files.

---
+++ Core/include/pch.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+// Windows
+#include <windows.h>
+
+
+// STL
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <functional>
+#include <optional>
+#include <filesystem>
+#include <format>
+#include <exception>
+#include <concepts>
+#include <type_traits>
+
+#include <wrl.h>
+#include <shellapi.h>
+#include <cmath>
+#include <cstdint>
---
+++ Sandbox/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <Application.hpp>
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, const int nShowCmd)
+{
+
+	Application app;
+
+	OutputDebugStringA("Hello, world !\n");
+
+	return 0;
+}
