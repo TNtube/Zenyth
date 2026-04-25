311101dd36c0aef5e66e24a0e4f92da745a09241
TNtube <tntube.dev@gmail.com>
2026-04-25 22:42:23 +0900

    Add math header for glm

---
+++ Core/include/Math.hpp
@@ -0,0 +1,7 @@
+#define GLM_FORCE_LEFT_HANDED       // D3D12
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE // NDC depth 0..1
+#define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
